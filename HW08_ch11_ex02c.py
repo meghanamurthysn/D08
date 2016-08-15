3bc7dd605e32da15ede7c20b085de6373d24949b
meghanamurthysn <meghanamurthy@berkeley.edu>
2016-08-15 06:23:42 -0700

    HW08_ch11_ex02c.py_v01

--- HW08_ch11_ex02c.py
+++ HW08_ch11_ex02c.py
@@ -18,22 +18,66 @@ def reverse_lookup_old(d, v):
             return k
     raise ValueError
 
-
 def reverse_lookup_new(d, v):
-    pass
-
+    list_keys = []
+    for k in d:
+	    if d[k] == v:
+		    list_keys.append(k)
+    return list_keys
+    raise ValueError
 
 ###############################################################################
 # INSERT COMPLETED CODE FROM HW08_ch11_ex02a BELOW: ###########################
 ###############################################################################
+pledge_histogram = {}
 
+
+def histogram_old(s):
+    d = dict()
+    for c in s:
+        d[c] = d.get(c, 0) + 1
+    return d
+
+
+def histogram_new(s):
+    d = dict()
+    for word in s:
+        d[word] = d.get(word, 0) + 1
+    pledge_histogram = d    
+    return d
+
+
+def get_pledge_list():
+    """ Opens pledge.txt and converts to a list, each item is a word in
+    the order it appears in the original file. returns the list.
+    """
+    # Your code here.
+    with open("pledge.txt", "r") as pledge:
+        pledge_list = []
+        pledge_list = [word for word in pledge.read().split()]
+        #Alternative method of nested list comprehension
+        #pledge_list = [word for line in pledge.readlines() for word in line.split()]
+        #below are the for loops for the nested list comprehension
+        #for line in pledge.readlines():
+        #    for word in line.split():
+        #        pledge_list.append(word)"""
+    return pledge_list
+
+pledge_histogram = histogram_new(get_pledge_list())
+
+###############################################################################
+def main():  # DO NOT CHANGE BELOW
+    print(histogram_new(get_pledge_list()))
+
+if __name__ == '__main__':
+    main()
 
 ###############################################################################
 # INSERT COMPLETED CODE FROM HW08_ch11_ex02a BELOW: ###########################
 ###############################################################################
 def main():   # DO NOT CHANGE BELOW
-    print(reverse_lookup_new(pledge_histogram, "1"))
-    print(reverse_lookup_new(pledge_histogram, "9"))
+    print(reverse_lookup_new(pledge_histogram, 1))
+    print(reverse_lookup_new(pledge_histogram, 9))
     print(reverse_lookup_new(pledge_histogram, "Python"))
 
 if __name__ == '__main__':
